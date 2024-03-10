6c05abac14d2d499812c2fccd3aedea1a8c215e3
Ashish1692 <ashishmoghe6@gmail.com>
2024-03-10 15:49:39 +0530

    css updated

--- Components/WagesList.jsx
+++ Components/WagesList.jsx
@@ -19,47 +19,47 @@ export default async function WagesList() {
     return (
         <>
             {recs.map((t) => (
-                <div key={t._id} className="grid_lay m-4 max-w-screen-sm bg-gray-400 rounded-lg overflow-hidden sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl xl:max-w-screen-2xl">
+                <div key={t._id} className="m-4 max-w-screen-sm bg-gray-400 rounded-lg overflow-hidden sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl xl:max-w-screen-2xl grid_lay ">
                     <div className="item">
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded">User</div>
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded">{t.user}</div>
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded">User</div>
+                            <div className="bg-green-300 sub_sub_item  text-green-800 rounded">{t.user}</div>
                         </div>
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded">Amount</div>
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded">{t.amount}</div>
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded">Amount</div>
+                            <div className="bg-green-300 sub_sub_item  text-green-800 rounded">{t.amount}</div>
                         </div>
                     </div>
                     <div className="item">
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded">Category</div>
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded">{t.category}</div>
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded">Category</div>
+                            <div className="bg-green-300 sub_sub_item  text-green-800 rounded">{t.category}</div>
                         </div>
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded">Date</div>
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded">{t.date}</div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded">Pay Method</div>
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded">{t.pay_meth}</div>
-                        </div>
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded">Pay Type</div>
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded">{t.pay_type}</div>
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded">Date</div>
+                            <div className="bg-green-300 sub_sub_item  text-green-800 rounded">{t.date}</div>
                         </div>
                     </div>
                     <div className="item">
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded">Description</div>
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded">{t.description}</div>
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded">Pay Method</div>
+                            <div className="bg-green-300 sub_sub_item  text-green-800 rounded">{t.pay_meth}</div>
                         </div>
-                        <div className="sub_item bg-gray-200 rounded border border-gray-300 flex justify-between items-center">
-                            <div className="sub_sub_item bg-blue-300 text-blue-800 rounded"><Link href={`/edit/${t._id}`} className="text-yellow-900">
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded">Pay Type</div>
+                            <div className="bg-green-300 sub_sub_item  text-green-800 rounded">{t.pay_type}</div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded">Description</div>
+                            <div className="bg-green-300 sub_sub_item  text-green-800 rounded">{t.description}</div>
+                        </div>
+                        <div className="bg-gray-200 rounded border border-gray-300 sub_item flex justify-between items-center">
+                            <div className="bg-blue-300  sub_sub_item text-blue-800 rounded"><Link href={`/edit/${t._id}`} className="text-yellow-900">
                                 <HiPencilAlt size={24} />                           </Link></div>
 
-                            <div className="sub_sub_item bg-green-300 text-green-800 rounded"><RemoveBtn id={t._id} /></div>
+                            <div className="bg-green-300 sub_sub_item text-green-800 rounded"><RemoveBtn id={t._id} /></div>
 
                         </div>
                     </div>
